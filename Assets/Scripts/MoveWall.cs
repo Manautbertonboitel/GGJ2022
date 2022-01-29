87a20229c91baf7876bba56039b9a55a83b56660
Manaut <manaut.bertonboitel@outlook.fr>
2022-01-29 13:40:51 +0100

    ADD Character Controller && Wall Animations

---
+++ Assets/Scripts/MoveWall.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWall : MonoBehaviour
+{
+    public GameObject WhiteWalls, BlackWalls;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(WhiteWalls.GetComponent<Wall>().status)
+        {
+            LowerWall(WhiteWalls);
+            WhiteWalls.GetComponent<Wall>().status = false;
+        } else
+        {
+            AscendWall(WhiteWalls);
+            WhiteWalls.GetComponent<Wall>().status = true;
+        }
+
+        if (BlackWalls.GetComponent<Wall>().status)
+        {
+            LowerWall(BlackWalls);
+            BlackWalls.GetComponent<Wall>().status = false;
+        }
+        else
+        {
+            AscendWall(BlackWalls);
+            BlackWalls.GetComponent<Wall>().status = true;
+        }
+    }
+
+    void LowerWall(GameObject wall)
+    {
+        wall.GetComponent<Animator>().Play("LowerWall");
+    }
+
+    void AscendWall(GameObject wall)
+    {
+        wall.GetComponent<Animator>().Play("AscendWall");
+    }
+}
